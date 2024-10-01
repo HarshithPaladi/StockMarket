1115188a12639722dc96f542bc1dab92ec324209
Saravanan K S <saravananks@presidio.com>
2024-10-01 15:37:10 +0530

    feat: added more mock data

--- src/store/data.js
+++ src/store/data.js
@@ -38,5 +38,45 @@ export const STOCK_LIST = [
         id: 8,
         name: 'Netflix, Inc.',
         price: 533.84,
+    },
+    {
+        id: 9,
+        name: 'Adobe Inc.',
+        price: 620.57,
+    },
+    {
+        id: 10,
+        name: 'Salesforce.com Inc.',
+        price: 251.23,
+    },
+    {
+        id: 11,
+        name: 'Intel Corporation',
+        price: 54.36,
+    },
+    {
+        id: 12,
+        name: 'PayPal Holdings Inc.',
+        price: 293.27,
+    },
+    {
+        id: 13,
+        name: 'Advanced Micro Devices, Inc.',
+        price: 102.94,
+    },
+    {
+        id: 14,
+        name: 'Qualcomm Inc.',
+        price: 143.56,
+    },
+    {
+        id: 15,
+        name: 'Cisco Systems, Inc.',
+        price: 55.42,
+    },
+    {
+        id: 16,
+        name: 'Oracle Corporation',
+        price: 88.75,
     }
 ]
