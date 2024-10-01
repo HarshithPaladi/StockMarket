8602eb5db1b44e75867ead5880c883c06531ff31
Harshith Paladi <hpaladi@presidio.com>
2024-10-01 15:21:49 +0530

    feat - Added more stocks

--- src/store/data.js
+++ src/store/data.js
@@ -13,5 +13,30 @@ export const STOCK_LIST = [
         id: 3,
         name: 'Amazon.com Inc.',
         price: 3372.20,
+    },
+    {
+        id: 4,
+        name: 'Alphabet Inc.',
+        price: 2738.27,
+    },
+    {
+        id: 5,
+        name: 'Facebook, Inc.',
+        price: 355.64,
+    },
+    {
+        id: 6,
+        name: 'Tesla, Inc.',
+        price: 688.99,
+    },
+    {
+        id: 7,
+        name: 'NVIDIA Corporation',
+        price: 195.55,
+    },
+    {
+        id: 8,
+        name: 'Netflix, Inc.',
+        price: 533.84,
     }
 ]
